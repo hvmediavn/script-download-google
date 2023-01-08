a9c44690c321d675f7f87f82c5a5d60b80b74b7e
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-01-08 23:29:57 +0700

    Update Script-download-google.sh

--- Script-download-google.sh
+++ Script-download-google.sh
@@ -2,7 +2,7 @@
 ## Author : Hoang Nam
 ## Last Updated : 30/12/2022
 ## Company : AZDIG
-
+pwdd=`pwd`
 cat << "EOF"
  _   _                                          _   _
 | \ | | __ _ _   _ _   _  ___ _ __             | \ | | __ _ _ __ ___
@@ -18,3 +18,4 @@ echo "! Script download google !"
 read -p "Nhap ten file can down : " file
 read -p "Nhap ID cua googledirver : " idgoogle
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${idgoogle}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${idgoogle}" -O ${file} && rm -rf /tmp/cookies.txt
+rm -f {pwdd}/Script-download-google.sh
