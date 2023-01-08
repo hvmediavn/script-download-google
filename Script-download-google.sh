d6580828f38c4557ac85bf75a038bd387fe3c131
skinsnguyen <72654840+skinsnguyen@users.noreply.github.com>
2023-01-08 23:32:03 +0700

    Update Script-download-google.sh

--- Script-download-google.sh
+++ Script-download-google.sh
@@ -18,4 +18,4 @@ echo "! Script download google !"
 read -p "Nhap ten file can down : " file
 read -p "Nhap ID cua googledirver : " idgoogle
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${idgoogle}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${idgoogle}" -O ${file} && rm -rf /tmp/cookies.txt
-rm -f {pwdd}/Script-download-google.sh
+rm -f ${pwdd}/Script-download-google.sh
